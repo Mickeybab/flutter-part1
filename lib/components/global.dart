878eadc2aff417414e0573ab5d365814cb643c16
Enzo PERROT <enzo@ouisnap.com>
2021-02-06 23:52:24 +0100

    Add text colors switch

--- lib/components/global.dart
+++ lib/components/global.dart
@@ -1,5 +1,20 @@
 library my_prj.globals;
 
+import 'package:flutter/material.dart';
+
 import 'myTheme.dart';
 
 MyTheme currentTheme = MyTheme();
+
+//ThemeData darkTheme = ThemeData(
+//    accentColor: Colors.orange,
+//    primaryColor: Colors.white,
+//    brightness: Brightness.dark);
+
+//ThemeData lightTheme = ThemeData(
+//    accentColor: Colors.orange,
+//    primaryColor: Colors.black,
+//    brightness: Brightness.light);
+
+ThemeData darkTheme = ThemeData.dark();
+ThemeData lightTheme = ThemeData.light();
