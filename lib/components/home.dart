34e328ace1dd4afa6af6dfea1cb2d515886dbe46
hugo.maurinier@epitech.eu <hugo.maurinier@epitech.eu>
2021-02-07 13:10:55 +0100

    [ADD] Home Page animation

--- lib/components/home.dart
+++ lib/components/home.dart
@@ -3,28 +3,37 @@ import 'package:flutter_epitech/components/soundRow.dart';
 import 'soundRow.dart';
 
 class Home extends StatelessWidget {
+  Future<double> get _height => Future<double>.value(1000);
   @override
   Widget build(BuildContext context) {
-    return 
-    Container(
-      color: Color(0xFFBD40),
-      constraints: BoxConstraints.expand(),
-      child: Column(
-        children: <Widget>[
-          SoundRow(
-            soundTitle1: "Geese Beat",
-            soundFile1: "sound/GeeseBeat.mp3",
-            soundTitle2: "Turtle Moan",
-            soundFile2: "sound/TurtleMoan.wav",
+    return FutureBuilder<double>(
+      future: _height,
+      initialData: 300,
+      builder: (context, snapshot) {
+        return AnimatedContainer(
+          color: Color(0xFFBD40),
+          duration: Duration(seconds: 2),
+          height: snapshot.data,
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                SoundRow(
+                  soundTitle1: "Geese Beat",
+                  soundFile1: "sound/GeeseBeat.mp3",
+                  soundTitle2: "Turtle Moan",
+                  soundFile2: "sound/TurtleMoan.wav",
+                ),
+                SoundRow(
+                  soundTitle1: "Knock",
+                  soundFile1: "sound/Knock.wav",
+                  soundTitle2: "Funny Sounds",
+                  soundFile2: "sound/FunnySounds.wav",
+                ),
+              ],
+            ),
           ),
-          SoundRow(
-            soundTitle1: "Knock",
-            soundFile1: "sound/Knock.wav",
-            soundTitle2: "Funny Sounds",
-            soundFile2: "sound/FunnySounds.wav",
-          ),
-        ],
-      ),
+        );
+      }
     );
   }
 }
